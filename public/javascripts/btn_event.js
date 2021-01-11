4d0a4cabfe814dc64d992377d4a285e89eca66bb
KimJeongYun <kjyjh9930@gmail.com>
2021-01-11 18:46:19 +0900

    feat : 페이지 교체 기능 추가 [2021-01-11]

--- public/javascripts/btn_event.js
+++ public/javascripts/btn_event.js
@@ -1,22 +1,36 @@
 'use strict';
 
+let menu;
 let intro_btn;
 let product_btn;
+let active_page;
+let current_page;
+
+let fadeout = function (index) {
+  console.log(active_page.item(index));
+};
+
+let fadein = function (index) {
+  console.log(active_page.item(index));
+};
 
 let btn_click_event = function (event) {
-  let obj = event.target.id;
-  if (obj === intro_btn.id) {
-    alert('소개버튼클릭');
-  }
-  if (obj === product_btn.id) {
-    alert('작품버튼클릭');
+  let obj = event.target;
+  if (obj == menu.item(0)) {
+    console.log('동일');
+    obj.style;
+  } else {
+    console.log('비동일');
   }
 };
 
 let init = function () {
-  intro_btn = document.getElementById('intro_btn');
+  active_page = document.getElementsByClassName('active_page');
+  current_page = active_page.item(0);
+  menu = document.getElementsByClassName('menu-btn');
+  intro_btn = menu.item(0);
+  product_btn = menu.item(1);
   intro_btn.addEventListener('click', btn_click_event);
-  product_btn = document.getElementById('product_btn');
   product_btn.addEventListener('click', btn_click_event);
 };
 
