6f712641dc01df4ea61979c7b1db9fc2cd8a951e
KimJeongYun <kjyjh9930@gmail.com>
2021-01-12 13:11:01 +0900

    feat : 버튼 클릭 이벤트 처리 완료 [2021-01-12]

--- public/javascripts/btn_event.js
+++ public/javascripts/btn_event.js
@@ -3,6 +3,7 @@
 let menu;
 let intro_btn;
 let product_btn;
+let clicked;
 let active_page;
 let current_page;
 
@@ -22,8 +23,29 @@ let btn_click_event = function (event) {
     item.style.removeProperty('color');
     item.style.removeProperty('border-bottom');
   }
+  clicked = event.target;
   event.target.style.color = bg_color;
   event.target.style.borderBottomColor = bg_color;
+};
+
+let btn_over_event = function (event) {
+  for (let item of menu) {
+    item.style.removeProperty('border-bottom');
+  }
+  event.target.style.borderBottomColor = bg_color;
+};
+
+let btn_out_event = function (event) {
+  for (let item of menu) {
+    item.style.removeProperty('border-bottom');
+  }
+  clicked.style.borderBottomColor = bg_color;
+};
+
+let add_event = function (btn) {
+  btn.addEventListener('click', btn_click_event);
+  btn.addEventListener('mouseover', btn_over_event);
+  btn.addEventListener('mouseout', btn_out_event);
 };
 
 let init = function () {
@@ -32,8 +54,9 @@ let init = function () {
   menu = document.getElementsByClassName('menu-btn');
   intro_btn = menu.item(0);
   product_btn = menu.item(1);
-  intro_btn.addEventListener('click', btn_click_event);
-  product_btn.addEventListener('click', btn_click_event);
+
+  add_event(intro_btn);
+  add_event(product_btn);
 };
 
 window.onload = init;
