d861ed818650967aa780caefb5c749a9a8d26505
KimJeongYun <kjyjh9930@gmail.com>
2021-01-28 23:18:09 +0900

    style : 타이틀 이미지 추가 [2021-01-28]

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Readme.JY' });
+  res.render('index', { title: 'README.jy' });
 });
 
 module.exports = router;
