24e2e5514875f303304e70208648d6d35bcad9b9
김정윤 <kimjeongyun@gimjeong-yun-ui-MacBookAir.local>
2021-03-15 11:38:36 +0900

    feat : 프로젝트 이미지 미리보기 박스 추가 [2020-03-15]

--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -2,36 +2,7 @@
 var App = window.App || {};
 
 App.preview = (function () {
-  let menu;
-  let img;
-
-  let _overEvent = function (event) {
-    let index = Number(App.main.getItemIndex(event, menu)) + Number(1);
-    let element = img.item(index);
-    for (let item of img) {
-      item.style.display = 'none';
-    }
-    element.style.display = 'block';
-  };
-
-  let _outEvent = function () {
-    if (img.item(0).style.display == 'block') {
-      return;
-    }
-    for (let item of img) {
-      item.style.display = 'none';
-    }
-    img.item(0).style.display = 'block';
-  };
-
   return {
-    run: function () {
-      menu = document.querySelectorAll('.product');
-      img = document.querySelectorAll('.project-img');
-      for (let item of menu) {
-        App.main.addEvent(item, 'mouseover', _overEvent);
-        App.main.addEvent(item, 'mouseout', _outEvent);
-      }
-    },
+    run: function () {},
   };
 })();
--- public/javascripts/previewImg.js
+++ public/javascripts/previewImg.js
@@ -4,6 +4,7 @@ var App = window.App || {};
 App.previewImg = (function () {
   let menu;
   let previewboxs;
+  let pos;
 
   let _overEvent = function (event) {
     let index = Number(App.main.getItemIndex(event, menu)) + Number(0);
@@ -15,13 +16,38 @@ App.previewImg = (function () {
     previewboxs.item(index).style.display = 'none';
   };
 
+  let _moveEvent = function (event) {
+    let index = Number(App.main.getItemIndex(event, menu)) + Number(0);
+    previewboxs.item(index).style.left = pos.positionX + 30 + 'px';
+    previewboxs.item(index).style.top = pos.positionY + 'px';
+  };
+
+  function get_position_of_mousePointer(event) {
+    event = event || window.event;
+
+    var x = 0; // 마우스 포인터의 좌측 위치
+    var y = 0; // 마우스 포인터의 위쪽 위치
+    x = event.clientX;
+    y = event.clientY;
+    return { positionX: x, positionY: y };
+  }
+
+  function trackPointer(event) {
+    pos = get_position_of_mousePointer(event);
+  }
+
   return {
     run: function () {
       menu = document.querySelectorAll('.product');
       previewboxs = document.querySelectorAll('.previewbox');
+      let body = document.body;
+
+      App.main.addEvent(body, 'mousemove', trackPointer);
+
       for (let item of menu) {
         App.main.addEvent(item, 'mouseover', _overEvent);
         App.main.addEvent(item, 'mouseout', _outEvent);
+        App.main.addEvent(item, 'mousemove', _moveEvent);
       }
     },
   };
